61421f55c8bbb0048bc3e620f8e383de8cd46855
Olexandr-Dobra <olexandrdobra@hotmail.com>
2020-05-17 23:21:48 +0300

    implement sign in

--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { SignInService } from 'src/app/service/signIn.service';
+import { SignInDto } from 'src/app/model/signIn-dto';
+import { Router } from '@angular/router';
+import { SignUpDto } from 'src/app/model/signUp-dto';
 
 declare var $: any;
 
@@ -8,18 +13,40 @@ declare var $: any;
   styleUrls: ['./authorization.component.css']
 })
 export class AuthorizationComponent implements OnInit {
+  usernameControl = new FormControl('', [Validators.required]);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordControl = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor(private signInService: SignInService, private router: Router) { }
 
   ngOnInit(): void {
+    debugger
     $(document).ready(() => {
-      $(".info-item .btn").click(function(){
+      $(".info-item .btn").click(function () {
         $(".container").toggleClass("log-in");
       });
-      $(".container-form .btn").click(function(){
+      $(".container-form .btn").click(function () {
         $(".container").addClass("active");
       });
     });
   }
 
+  signin() {
+    if (this.emailControl.valid && this.passwordControl.valid) {
+      const signInModel = new SignInDto();
+      signInModel.Password = this.passwordControl.value;
+      signInModel.UserName = this.emailControl.value;
+      signInModel.RememberMe = true;
+      this.signInService.signIn(signInModel).subscribe(() => this.router.navigate(['/main']));
+    }
+  }
+  signup() {
+    if (this.emailControl.valid && this.passwordControl.valid && this.usernameControl.valid) {
+      const signUpModel = new SignUpDto();
+      signUpModel.Password = this.passwordControl.value;
+      signUpModel.UserName = this.usernameControl.value;
+      signUpModel.Email = this.emailControl.value;
+      this.signInService.signUp(signUpModel).subscribe(() => this.router.navigate(['/main']));
+    }
+  }
 }
